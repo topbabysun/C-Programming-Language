6e62fb38187b0cc04e75cf179aab2937fbb46089
Beibei Sun <BEIBEISUN@Beibeis-MacBook-Air.local>
2016-11-25 13:14:26 +0800

    search for a substring and calculate frequency

---
+++ Chapter_4/4_1search.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#define MAXLINE 100
+
+int getline(char line[], int max);
+int strindex(char source[], char searchfor[]);
+char pattern[] = "ould"; /* pattern to be searched */
+/* find all lines matching pattern */
+main()
+{
+	char line[MAXLINE];
+	int found = 0;
+
+	while (getline(line, MAXLINE) > 0)
+		if (strindex(line, pattern) >= 0){
+			printf("%s", line);
+			found++;
+		}
+	return found;
+	
+}
+
+int getline(char s[], int lim)
+{
+	int c, i;
+
+	i = 0;
+	while(--lim > 0 && (c = getchar()) != EOF && c != '\n')
+		s[i++] = c;
+	if(c == '\n')
+		s[i++] = c;
+	s[i] = '\0';
+	return i;
+
+}
+/* a different solution to get a new line. */ 
+
+
+int strindex(char s[], char t[])
+{
+	
+	int i, j, k;
+
+	for(i = 0, s[i] !='\0'; i++){
+		for( j = i; k = 0; t[k] != '\0' && s[j] = t[k]; j++, k++)
+			;
+		if(k > 0 && t[k] == '\0') /* how to make sure the match is a complete pattern */
+			return i;
+	}
+	return -1;
+	
+}
